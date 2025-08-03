c3ce88d197390ee15077699264f5303beae3ac54
Dev-13552 <devrathour110@gmail.com>
2025-08-03 20:04:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function firstWord(s) {
   // your code here
+	if(s.length == 0){
+		return s;
+	}
+	let i = 0;
+	let output = ""
+	while(s[i] != " "){
+		output += s[i]
+		i++
+	}
+	return output
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
+
 alert(firstWord(s));
